d45565f50d34016a871a2e8f722fcddd4d4f16fc
samedovAV <alekcandr206@gmail.com>
2021-03-21 14:38:20 +0300

    Add. merch and fixes

--- send.php
+++ send.php
@@ -1,35 +1,38 @@
-<?php
-/* Здесь проверяется существование переменных */
-if (isset($_POST['name'])) {$phone = $_POST['name'];}
-if (isset($_POST['phone'])) {$name = $_POST['phone'];}
- 
-/* Сюда впишите свою эл. почту */
-$myaddres  = "alekcandr@yandex.ru"; // кому отправляем
- 
-/* А здесь прописывается текст сообщения, \n - перенос строки */
-$mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name";
- 
-/* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ обратного звонка'; // от кого
-$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
-ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="3; url=index.html">
-<title>Спасибо! Мы свяжемся с вами!</title>
-<meta name="generator">
-<script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
-/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
+<meta charset="UTF-8">
+<title>Document</title>
 </head>
 <body>
-<h1>Спасибо! Мы свяжемся с вами!</h1>
+
+<?php
+if(!empty($_POST['telephone'] ))
+{
+$to = "rundergroundmoscow@gmail.com";
+$from = "info@runderground.moscow";
+$subject = "Сборы Runderground";
+$message = 'ФИО: '.$_POST['name'].';
+            Дата рождения: '.$_POST['birthday'].'; 
+            Телефон: '.$_POST['telephone'].';
+            Email: '.$_POST['email'].';
+            Ссылка: '.$_POST['vk'].';
+            ';
+$headers = "Content-type: text/html; charset=UTF-8 \r\n";
+$headers .= "From: info@runderground.moscow>\r\n";
+$result = mail($to, $subject, $message, $headers);
+
+    if ($result){ 
+        echo "<p>Cообщение успешно отправлено. Пожалуйста, оставайтесь на связи</p>";
+    }
+    else{
+        echo "<p>Cообщение не отправленно. Пожалуйста, попрбуйте еще раз</p>";
+    }
+}
+else {
+echo "<p>Обязательные поля не заполнены. Введите номер телефона</p>";
+}
+?>
+
 </body>
 </html>
